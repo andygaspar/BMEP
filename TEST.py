38e096229dbf38c134394660d8e8c22556e0b51f
andgaspar <andrea.gasparin.87@gmail.com>
2022-06-04 23:10:55 +0200

    da sistemare ordine nodi

--- TEST.py
+++ TEST.py
@@ -1,6 +1,44 @@
 import numpy as np
 import string
 
+import copy
+import string
+
+import networkx as nx
+from matplotlib import pyplot as plt
+
+from alg_ import get_pardi
+from print_graph import get_graph
+import numpy as np
+
+T = np.array([[0, 3, 2, 5, 4, 5],
+              [3, 0, 3, 4, 3, 4],
+              [2, 3, 0, 5, 4, 5],
+              [5, 4, 5, 0, 3, 2],
+              [4, 3, 4, 3, 0, 3],
+              [5, 4, 5, 2, 3, 0]])
+
+T = np.array([[0, 4, 2, 4, 4, 4],
+              [4, 0, 4, 4, 4, 2],
+              [2, 4, 0, 4, 4, 4],
+              [4, 4, 4, 0, 2, 4],
+              [4, 4, 4, 2, 0, 4],
+              [4, 2, 4, 4, 4, 0]])
+
+# T = np.array([[0, 4, 2, 5, 5, 4, 4],
+#               [4, 0, 4, 5, 5, 2, 4],
+#               [2, 4, 0, 5, 5, 4, 4],
+#               [5, 5, 5, 0, 2, 5, 3],
+#               [5, 5, 5, 2, 0, 5, 3],
+#               [4, 2, 4, 5, 5, 0, 4],
+#               [4, 4, 4, 3, 3, 4, 0]])
+
+# T = np.array([[0, 4, 2, 4, 3],
+#               [4, 0, 4, 2, 3],
+#               [2, 4, 0, 4, 3],
+#               [4, 2, 4, 0, 3],
+#               [3, 3, 3, 3, 0]])
+
 T = np.array([[0, 4, 2, 5, 5, 4, 4],
               [4, 0, 4, 5, 5, 2, 4],
               [2, 4, 0, 5, 5, 4, 4],
@@ -10,34 +48,42 @@ T = np.array([[0, 4, 2, 5, 5, 4, 4],
               [4, 4, 4, 3, 3, 4, 0]])
 
 
-def step(leaf, T, labels):
-    assignment = []
-    match = np.where(T[leaf] == 2)[0]
-    if match.shape[0] > 0:
-        match = match[0]
-        if match < leaf:
-            chery = match
-            assignment.append(labels[leaf] + "->" + labels[chery])
-            T[:, chery] -= - 1
-            T[chery] -= 1
-            T[chery, chery] += 1
-            T[:, leaf] = - 1
-            T[leaf] = 1
+m = T.shape[0]
+labels = [s for s in string.ascii_uppercase[:m]]
+G = get_graph(T, labels)
+nx.draw(G, node_color=[G.nodes[node]["color"] for node in G.nodes],
+        with_labels=True, font_weight='bold')
+plt.show()
+
+
+sequence, g = get_pardi(G, labels)
+
+def build_pardi(sequence):
+    G = nx.Graph()
+    m = len(sequence) + 3
+    G.add_nodes_from([("A", {"color": "red"}), ("B", {"color": "red"}),
+                      ("C", {"color": "red"}), (m, {"color": "green"})])
+    G.add_edges_from([("A", m), ("B", m), ("C", m)])
+    nx.draw(G, node_color=[G.nodes[node]["color"] for node in G.nodes],
+            with_labels=True, font_weight='bold')
+    plt.show()
+    for k, node in enumerate(sequence):
+        if len(node[1]) < 2:
+            internal_node = m + 1 + k
+            G.add_nodes_from([(node[0], {"color": "red"}), (internal_node, {"color": "green"})])
+            removing_edge = list(G.edges(node[1][0]))[0]
+            G.remove_edge(removing_edge[0], removing_edge[1])
+            G.add_edges_from([(node[0], internal_node), (node[1][0], internal_node), (removing_edge[1], internal_node)])
+
+            print(removing_edge)
+            nx.draw(G, node_color=[G.nodes[node]["color"] for node in G.nodes],
+                    with_labels=True, font_weight='bold')
+            plt.show()
         else:
-            pass
-    else:
-        assignment.append(labels[leaf] + "->" + str(leaf - 2))
-        T[:, -1] -= 1
-
-    print(assignment)
-    return T
+            internal_node = m + 1 + k
+            G.add_nodes_from([(node[0], {"color": "red"}), (internal_node, {"color": "green"})])
+            G.remove_edge(node[1][0], node[1][1])
+            G.add_edges_from([(node[0], internal_node), (node[1][0], internal_node), (node[1][2], internal_node)])
 
 
-labels = [s for s in string.ascii_uppercase[:T.shape[0]]]
-
-T = step(T, labels)
-T = step(T, labels)
-T = step(T, labels)
-T = step(T, labels)
-
-print(T)
+build_pardi(sequence)
--- alg_.py
+++ alg_.py
@@ -7,99 +7,64 @@ from matplotlib import pyplot as plt
 from print_graph import get_graph
 import numpy as np
 
-T = np.array([[0, 3, 2, 5, 4, 5],
-              [3, 0, 3, 4, 3, 4],
-              [2, 3, 0, 5, 4, 5],
-              [5, 4, 5, 0, 3, 2],
-              [4, 3, 4, 3, 0, 3],
-              [5, 4, 5, 2, 3, 0]])
 
-T = np.array([[0, 4, 2, 4, 4, 4],
-              [4, 0, 4, 4, 4, 2],
-              [2, 4, 0, 4, 4, 4],
-              [4, 4, 4, 0, 2, 4],
-              [4, 4, 4, 2, 0, 4],
-              [4, 2, 4, 4, 4, 0]])
+def get_pardi(g, labels):
+    m = (len(g) + 2) // 2
+    nodes = labels + [i for i in range(m, 2 * m - 2)]
+    node_dict = dict(zip(nodes, range(len(nodes))))
+    node_dict_back = dict(zip(range(len(nodes)), nodes))
 
-# T = np.array([[0, 4, 2, 5, 5, 4, 4],
-#               [4, 0, 4, 5, 5, 2, 4],
-#               [2, 4, 0, 5, 5, 4, 4],
-#               [5, 5, 5, 0, 2, 5, 3],
-#               [5, 5, 5, 2, 0, 5, 3],
-#               [4, 2, 4, 5, 5, 0, 4],
-#               [4, 4, 4, 3, 3, 4, 0]])
+    ad_mat = np.zeros((len(nodes), len(nodes)))
 
-# T = np.array([[0, 4, 2, 4, 3],
-#               [4, 0, 4, 2, 3],
-#               [2, 4, 0, 4, 3],
-#               [4, 2, 4, 0, 3],
-#               [3, 3, 3, 3, 0]])
+    for edge in g.edges:
+        a, b = edge
+        ad_mat[node_dict[a], node_dict[b]] = 1
+        ad_mat[node_dict[b], node_dict[a]] = 1
 
-m = T.shape[0]
-labels = [s for s in string.ascii_uppercase[:m]]
-G = get_graph(T, labels)
-nx.draw(G, node_color=[G.nodes[node]["color"] for node in G.nodes],
-        with_labels=True, font_weight='bold')
-plt.show()
+    ad_mat_1 = copy.deepcopy(ad_mat)
 
-def get_pardi(mat)
-nodes = labels + [i for i in range(m, 2 * m - 2)]
-node_dict = dict(zip(nodes, range(len(nodes))))
-node_dict_back = dict(zip(range(len(nodes)), nodes))
+    leafs = nodes[:m]
+    leafs = leafs[::-1]
+    internal_nodes = []
+    pardi = []
+    for l in leafs[:-3]:
+        x = node_dict[l]
+        j = np.where(ad_mat_1[x] == 1)[0][-1]
+        internal_nodes.append(j)
+        ad_mat_1[j, x] = 0
+        i = np.where(ad_mat_1[j] == 1)[0][0]
+        if i < m:
+            print(l, "->", node_dict_back[i])
+            pardi.append((l, [node_dict_back[i]]))
+        else:
+            ii = np.where(ad_mat_1[j] == 1)[0][1]
+            print(l, "->", node_dict_back[i], node_dict[ii])
+            pardi.append((l, [node_dict_back[i], node_dict_back[ii]]))
+        k = np.where(ad_mat_1[j] == 1)[0][-1]
+        ad_mat_1[k, j] = 0
+        ad_mat_1[k, i] = 1
+        ad_mat_1[i, k] = 1
+        ad_mat_1[i, j] = 0
+        ad_mat_1[x] = 0
+        ad_mat_1[:, x] = 0
+        ad_mat_1[j] = 0
+        ad_mat_1[:, j] = 0
+        # g = nx.from_numpy_matrix(ad_mat_1)
+        # mapping = dict(zip(g, nodes))
+        # g = nx.relabel_nodes(g, mapping)
+        # nx.draw(g, node_color=["red" if i < m else "green" for i in range(2 * m - 2)],
+        #         with_labels=True, font_weight='bold')
+        # plt.show()
 
-ad_mat = np.zeros((len(nodes), len(nodes)))
-
-for edge in G.edges:
-    a, b = edge
-    ad_mat[node_dict[a], node_dict[b]] = 1
-    ad_mat[node_dict[b], node_dict[a]] = 1
-
-ad_mat_1 = copy.deepcopy(ad_mat)
-
-leafs = nodes[:m]
-leafs = leafs[::-1]
-internal_nodes = []
-pardi = []
-for l in leafs[:-3]:
-    x = node_dict[l]
-    j = np.where(ad_mat_1[x] == 1)[0][-1]
-    internal_nodes.append(j)
-    ad_mat_1[j, x] = 0
-    i = np.where(ad_mat_1[j] == 1)[0][0]
-    if i < m:
-        print(l, "->", node_dict_back[i])
-        pardi.append((l, node_dict_back[i]))
-    else:
-        ii = np.where(ad_mat_1[j] == 1)[0][1]
-        print(l, "->", node_dict_back[i], node_dict[ii])
-        pardi.append((l, [node_dict_back[i], node_dict_back[ii]]))
-    k = np.where(ad_mat_1[j] == 1)[0][-1]
-    ad_mat_1[k, j] = 0
-    ad_mat_1[k, i] = 1
-    ad_mat_1[i, k] = 1
-    ad_mat_1[i, j] = 0
-    ad_mat_1[x] = 0
-    ad_mat_1[:, x] = 0
-    ad_mat_1[j] = 0
-    ad_mat_1[:, j] = 0
-    g = nx.from_numpy_matrix(ad_mat_1)
-    mapping = dict(zip(g, nodes))
+    # print(internal_nodes)
+    map_nodes = dict(zip(internal_nodes[::-1], range(m + 1, 2*m - 2)))
+    mapping = dict(zip(g, [node if node not in internal_nodes else map_nodes[node] for node in g.nodes]))
     g = nx.relabel_nodes(g, mapping)
-    nx.draw(g, node_color=["red" if i < m else "green" for i in range(2 * m - 2)],
-            with_labels=True, font_weight='bold')
-    plt.show()
+    # nx.draw(G, node_color=[G.nodes[node]["color"] for node in G.nodes],
+    #         with_labels=True, font_weight='bold')
+    # plt.show()
 
-print(internal_nodes)
-map_nodes = dict(zip(internal_nodes[::-1], range(m + 1, 2*m - 2)))
-mapping = dict(zip(G, [node if node not in internal_nodes else map_nodes[node] for node in G.nodes ]))
-G = nx.relabel_nodes(G, mapping)
-nx.draw(G, node_color=[G.nodes[node]["color"] for node in G.nodes],
-        with_labels=True, font_weight='bold')
-plt.show()
-
-pardi = pardi[::-1]
-
-for p in pardi:
-    print(p)
+    pardi = pardi[::-1]
+    return pardi, g
 
 
