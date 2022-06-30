e4ee6ccb4e71bb80fb8ed650b381fca363730f7b
andrea <andygaspar@libero.it>
2022-06-30 14:30:42 +0200

    test finito

--- Net/test_net.py
+++ Net/test_net.py
@@ -1,3 +1,6 @@
+import copy
+
+import numpy as np
 import torch
 
 from Data_.Dataset.bmep_dataset import BMEP_Dataset
@@ -17,50 +20,31 @@ dataloader = DataLoader(dataset=data_, batch_size=batch_size, shuffle=True)
 
 # dgn = DGN_(8, 128, 128, 6)
 # dgn = GNN(num_inputs=2, h_dimension=512, hidden_dim=512, num_messages=7)
-dgn = GNN_2(num_inputs=2, h_dimension=512, hidden_dim=512, num_messages=7)
+dgn = GNN_2(num_inputs=2, h_dimension=512, hidden_dim=512, num_messages=7, network="Net/Nets/net3.68.pt")
 # y_hat = dgn.forward(adj_mats[0].unsqueeze(0), d_mats[0].unsqueeze(0), initial_masks[0].unsqueeze(0),
 #                     masks[0].unsqueeze(0))
 
-criterion = nn.CrossEntropyLoss()
-# criterion = nn.MSELoss()
 
-optimizer = optim.Adam(dgn.parameters(), lr=1e-5, weight_decay=1e-3)
 # optimizer = optim.SGD(dgn.parameters(), lr=1e-4, momentum=0.9)
-k, yy = None, None
-for epoch in range(10_000):
-    loss = None
+errors = []
+lenghts = []
+for epoch in range(10):
     for data in dataloader:
-        adj_mats, d_mats, initial_masks, masks, y = data
-        optimizer.zero_grad()
-        output, h = dgn(adj_mats, d_mats, initial_masks, masks)
-        # out, yy = output[masks > 0], y[masks > 0]
-        # loss = criterion(out, yy)
-        loss = criterion(output, y.view(y.shape[0], -1))
-
-
-        loss.backward()
-        # torch.nn.utils.clip_grad_norm_(dgn.parameters(), max_norm=0.001, norm_type=float('inf'))
-        # torch.nn.utils.clip_grad_norm_(dgn.parameters(), 1)
-        optimizer.step()
-    if epoch % 20 == 0:
         with torch.no_grad():
-
+            adj_mats, d_mats, initial_masks, masks, y = data
+            output, h = dgn(adj_mats, d_mats, initial_masks, masks)
             idx = torch.max(output, dim=-1)[1]
             prediction = torch.zeros_like(output)
             id = torch.tensor(range(idx.shape[0])).to("cuda:0")
             prediction[id, idx] = 1
             err = torch.sum(torch.abs(prediction - y.view(y.shape[0], -1)))
-            print(epoch, loss.item(), "error", err.item() / 2, "over", y.shape[0])
+            errors.append(err.item()/2)
+            lenghts.append(y.shape[0])
+            print(epoch, "error", err.item() / 2, "over", y.shape[0])
 
-        if epoch % 100 == 0:
-            with torch.no_grad():
+print("mean num errors", np.mean(errors))
+print("error rate ", np.sum(errors)/np.sum(lenghts))
 
-                o = (torch.matmul(h[0], h[0].permute(1, 0)) * masks[0])
-                j = list(masks[0].nonzero())
-                o = o[[x[0] for x in j], [x[1] for x in j]]
-                print(torch.mean(h[0], dim=-2))
-                print(torch.std(h[0], dim=-2))
-                print(j)
-                print(o, y.nonzero()[0], "\n")
+
 
 
---
+++ Net/train_net.py
@@ -0,0 +1,71 @@
+import copy
+
+import torch
+
+from Data_.Dataset.bmep_dataset import BMEP_Dataset
+import torch.optim as optim
+from torch import nn
+
+
+from torch.utils.data import DataLoader
+
+from Net.Nets.gnn_1 import GNN
+from Net.Nets.gnn_2 import GNN_2
+
+data_ = BMEP_Dataset()
+batch_size = 128
+dataloader = DataLoader(dataset=data_, batch_size=batch_size, shuffle=True)
+
+
+# dgn = DGN_(8, 128, 128, 6)
+# dgn = GNN(num_inputs=2, h_dimension=512, hidden_dim=512, num_messages=7)
+dgn = GNN_2(num_inputs=2, h_dimension=512, hidden_dim=512, num_messages=7)
+# y_hat = dgn.forward(adj_mats[0].unsqueeze(0), d_mats[0].unsqueeze(0), initial_masks[0].unsqueeze(0),
+#                     masks[0].unsqueeze(0))
+
+criterion = nn.CrossEntropyLoss()
+# criterion = nn.MSELoss()
+
+optimizer = optim.Adam(dgn.parameters(), lr=1e-5, weight_decay=1e-3)
+# optimizer = optim.SGD(dgn.parameters(), lr=1e-4, momentum=0.9)
+k, yy = None, None
+best_loss = 1e+4
+best_net = copy.deepcopy(dgn)
+for epoch in range(5_000):
+    loss = None
+    for data in dataloader:
+        adj_mats, d_mats, initial_masks, masks, y = data
+        optimizer.zero_grad()
+        output, h = dgn(adj_mats, d_mats, initial_masks, masks)
+        # out, yy = output[masks > 0], y[masks > 0]
+        # loss = criterion(out, yy)
+        loss = criterion(output, y.view(y.shape[0], -1))
+        loss.backward()
+        if loss.item() < best_loss:
+            best_loss = loss.item()
+            best_net = copy.deepcopy(dgn)
+        # torch.nn.utils.clip_grad_norm_(dgn.parameters(), max_norm=0.001, norm_type=float('inf'))
+        optimizer.step()
+    if epoch % 20 == 0:
+        with torch.no_grad():
+
+            idx = torch.max(output, dim=-1)[1]
+            prediction = torch.zeros_like(output)
+            id = torch.tensor(range(idx.shape[0])).to("cuda:0")
+            prediction[id, idx] = 1
+            err = torch.sum(torch.abs(prediction - y.view(y.shape[0], -1)))
+            print(epoch, loss.item(), "error", err.item() / 2, "over", y.shape[0], "  best", best_loss)
+
+        if epoch % 100 == 0:
+            with torch.no_grad():
+
+                o = (torch.matmul(h[0], h[0].permute(1, 0)) * masks[0])
+                j = list(masks[0].nonzero())
+                o = o[[x[0] for x in j], [x[1] for x in j]]
+                print(torch.mean(h[0], dim=-2))
+                print(torch.std(h[0], dim=-2))
+                print(j)
+                print(o, y.nonzero()[0], "\n")
+
+best_net.save_weights("Net/Nets/net" + str(int(best_loss * 100)/100))
+
