7c1aef6673151aa33286305bcaea06cd989508a5
andrea <andygaspar@libero.it>
2022-07-01 23:56:09 +0200

    pre merge

--- Net/test_net.py
+++ Net/test_net.py
@@ -1,3 +1,6 @@
+import numpy as np
+import torch
+
 from Data_.Dataset.bmep_dataset import BMEP_Dataset
 
 from torch.utils.data import DataLoader
@@ -10,31 +13,31 @@ batch_size = 128
 dataloader = DataLoader(dataset=data_, batch_size=batch_size, shuffle=True)
 
 
-dgn = GNN(num_inputs=2, h_dimension=512, hidden_dim=512, num_messages=7, network="Net/Nets/net3.68.pt")
+dgn = GNN(num_inputs=2, h_dimension=512, hidden_dim=512, num_messages=7, network="Net/Nets/net3.66.pt")
 
-d = data_.d_mats[0]
-net_solver = NetSolver(d, dgn)
-net_solver.solve()
-print(net_solver.solution)
+# d = data_.d_mats[0]
+# net_solver = NetSolver(d, dgn)
+# net_solver.solve()
+# print(net_solver.solution)
 
-# errors = []
-# lenghts = []
-# for epoch in range(10):
-#     for data in dataloader:
-#         with torch.no_grad():
-#             adj_mats, d_mats, initial_masks, masks, y = data
-#             output, h = dgn(adj_mats, d_mats, initial_masks, masks)
-#             idx = torch.max(output, dim=-1)[1]
-#             prediction = torch.zeros_like(output)
-#             id = torch.tensor(range(idx.shape[0])).to("cuda:0")
-#             prediction[id, idx] = 1
-#             err = torch.sum(torch.abs(prediction - y.view(y.shape[0], -1)))
-#             errors.append(err.item()/2)
-#             lenghts.append(y.shape[0])
-#             print(epoch, "error", err.item() / 2, "over", y.shape[0])
-#
-# print("mean num errors", np.mean(errors))
-# print("error rate ", np.sum(errors)/np.sum(lenghts))
+errors = []
+lenghts = []
+for epoch in range(10):
+    for data in dataloader:
+        with torch.no_grad():
+            adj_mats, d_mats, initial_masks, masks, y = data
+            output, h = dgn(adj_mats, d_mats, initial_masks, masks)
+            idx = torch.max(output, dim=-1)[1]
+            prediction = torch.zeros_like(output)
+            id = torch.tensor(range(idx.shape[0])).to("cuda:0")
+            prediction[id, idx] = 1
+            err = torch.sum(torch.abs(prediction - y.view(y.shape[0], -1)))
+            errors.append(err.item()/2)
+            lenghts.append(y.shape[0])
+            print(epoch, "error", err.item() / 2, "over", y.shape[0])
+
+print("mean num errors", np.mean(errors))
+print("error rate ", np.sum(errors)/np.sum(lenghts))
 
 
 
--- NetSolver/test_net_solver.py
+++ NetSolver/test_net_solver.py
@@ -13,7 +13,7 @@ batch_size = 128
 dataloader = DataLoader(dataset=data_, batch_size=batch_size, shuffle=True)
 
 
-dgn = GNN(num_inputs=2, h_dimension=512, hidden_dim=512, num_messages=7, network="Net/Nets/net3.68.pt")
+dgn = GNN(num_inputs=2, h_dimension=512, hidden_dim=512, num_messages=7, network="Net/Nets/net3.66.pt")
 for i in range(30):
     d = data_.d_mats[i*3]
     net_solver = NetSolver(d, dgn)
@@ -24,6 +24,6 @@ for i in range(30):
     # print(instance.adj_mat_solution)
     # print(d[:6, :6])
 
-    print("correct", np.array_equal(net_solver.solution, instance.adj_mat_solution))
+    print("correct", np.array_equal(net_solver.solution, instance.adj_mat_solution), instance.problem.status)
 
 
