edf105c144b2f972ded89d241a199cbf5f764428
andrea <andygaspar@libero.it>
2023-01-12 14:20:19 +0100

    sistemato spark

--- Data_/csv_/Dist/Dist/data_spark.py
+++ Data_/csv_/Dist/Dist/data_spark.py
@@ -15,14 +15,17 @@ def get_words(seq_str, k):
 
     return words
 
-@udf(IntegerType())
-def count_words(seq, word):
-    seq_ = Seq(seq)
-    return seq_.count_overlap(word)
+
+def fun(row):
+    res = []
+    seq = Seq(row.sequence)
+    for word in all_words_set:
+        res.append((row.taxon, word, seq.count_overlap(word)))
+    return res
 
 spark = SparkSession.builder.getOrCreate()
 
-df_pandas = pd.read_csv("Data_/csv_/Dist/Dist/Covid19_sequences.csv")
+df_pandas = pd.read_csv("Data_/csv_/Dist/Dist/Covid19_sequences.csv").iloc[:20]
 ddf_pandas = dd.from_pandas(df_pandas, npartitions=4)
 dask_series = ddf_pandas['sequence'].apply(get_words, args=(8,), meta=('x', 'int'))
 ddf_pandas['word_set'] = dask_series
@@ -32,12 +35,12 @@ all_words_set = set().union(*df_pandasf.word_set.to_list())
 
 
 
-df = spark.read.csv('Data_/csv_/Dist/Dist/Covid19_sequences.csv', inferSchema=True, header=True)
+df = spark.read.csv('Data_/csv_/Dist/Dist/Covid19_sequences.csv', inferSchema=True, header=True).limit(20)
 # df = df.withColumn('word_set', get_words(f.col('sequence'), f.lit(8)))
 
 print("here *************************************")
-i = 0
-for word in list(all_words_set)[:1000]:
-    df = df.withColumn(word, count_words(f.col('sequence'), f.lit(word)))
-    i += 1
-df.show()
+
+
+out = df.rdd.flatMap(lambda r: fun(r))
+df_pandas = out.toDF(["taxon", "word", "count"]).toPandas()
+df_pandas.taxon.unique()
