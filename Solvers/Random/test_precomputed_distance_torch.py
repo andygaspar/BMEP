ad2e6af5f8d5dd43dbebed7f4e7667fe473d9a2f
andgaspar <andrea.gasparin.87@gmail.com>
2023-02-16 18:30:19 +0100

    implementato calcolo d in tre modi

--- Solvers/Random/test_precomputed_distance_torch.py
+++ Solvers/Random/test_precomputed_distance_torch.py
@@ -44,8 +44,24 @@ class PrecomputeTorch(Solver):
         # self.T = self.get_tau(self.solution)
         self.T = T
         print('build time', time.time() - t)
+        t = time.time()
         self.subtree_dist = self.compute_dist()
+        print(time.time() - t)
+        t = time.time()
+        subtrees_dist = self.compute_dist_1()
+        print(time.time() - t)
+        t = time.time()
+        sub_dist = self.compute_dist_2()
+        print(time.time() - t)
+        print(torch.equal(self.subtree_dist, sub_dist*2))
+
+        print(self.subtree_dist)
+        print(sub_dist*2)
         # self.T_new = T.numpy().astype(np.int32)[:self.n_taxa, :self.n_taxa]
+        for i in range(2*self.m -3):
+            for j in range(2 * self.m - 3):
+                if self.subtree_dist[i,j] != sub_dist[i,j]*2:
+                    print(self.subtree_dist[i,j] , sub_dist[i,j])
 
         # print('equal', np.array_equal(self.T, self.T_new))
 
@@ -152,28 +168,65 @@ class PrecomputeTorch(Solver):
         return T
 
     def compute_dist(self):
-        dist_mat = torch.zeros((self.m, self.m), device=self.device)
-        # add pre-computation TO DO
-        # d = self.d / 2** self.T[:self.n_taxa, :self.n_taxa]
+        dist_mat = torch.zeros((2*(2*self.n_taxa - 3), 2*(2*self.n_taxa - 3)), dtype=torch.float64, device=self.device)
         d = self.d * self.powers[self.T[:self.n_taxa, :self.n_taxa]]
-        for i in range(self.m):
-            for j in range(i + 1, self.m):
-                a = torch.nonzero(self.subtrees_mat[i, :, j])[0][0]
-                A = torch.nonzero(self.subtrees_mat[a, i, :self.n_taxa]).T.squeeze(0)
-                b = torch.nonzero(self.subtrees_mat[j, :, i])[0][0]
-                B = torch.nonzero(self.subtrees_mat[b, j, :self.n_taxa]).T.squeeze(0)
-                idxs = torch.cartesian_prod(A, B)
-                dist_mat[i, j] = (d[idxs[:, 0], idxs[:, 1]] ).sum()
+
+        edges = torch.nonzero(self.solution)
+        idx_dict = dict(zip([tuple(edge.tolist()) for edge in edges], range(edges.shape[0])))
+        for edge in edges:
+            for o_edge in edges:
+                if (self.subtrees_mat[edge[0], edge[1], :] * self.subtrees_mat[o_edge[0], o_edge[1], :]).sum() == 0:
+                    A = torch.nonzero(self.subtrees_mat[edge[0], edge[1], :self.n_taxa], as_tuple=True)[0]
+                    B = torch.nonzero(self.subtrees_mat[o_edge[0], o_edge[1], :self.n_taxa], as_tuple=True)[0]
+                    idxs = torch.cartesian_prod(A, B)
+                    dist_mat[idx_dict[(edge[0].item(), edge[1].item())], idx_dict[(o_edge[0].item(), o_edge[1].item())]] = (d[idxs[:, 0], idxs[:, 1]] ).sum()
 
         dist_mat = dist_mat + dist_mat.T
 
         return dist_mat
 
+    def compute_dist_1(self):
+        dist_mat = torch.zeros((2*(2*self.n_taxa - 3), 2*(2*self.n_taxa - 3)), dtype=torch.float64, device=self.device)
+        d = self.d * self.powers[self.T[:self.n_taxa, :self.n_taxa]]
+        edges = torch.nonzero(self.solution)
+        idx_dict = dict(zip([tuple(edge.tolist()) for edge in edges], range(edges.shape[0])))
+        s_mat = self.subtrees_mat[edges[:, 0], edges[:, 1], :].clone()
+
+        for edge in edges:
+            for o_edge in edges:
+                if (self.subtrees_mat[edge[0], edge[1], :] * self.subtrees_mat[o_edge[0], o_edge[1], :]).sum() == 0:
+                    idx_1 = idx_dict[(edge[0].item(), edge[1].item())]
+                    idx_2 = idx_dict[(o_edge[0].item(), o_edge[1].item())]
+                    A = torch.nonzero(s_mat[idx_1, :self.n_taxa], as_tuple=True)[0]
+                    B = torch.nonzero(s_mat[idx_2, :self.n_taxa], as_tuple=True)[0]
+                    idxs = torch.cartesian_prod(A, B)
+                    dist_mat[idx_1, idx_2] = (d[idxs[:, 0], idxs[:, 1]]).sum()
+
+        dist_mat = dist_mat + dist_mat.T
+
+        return dist_mat
+
+    def compute_dist_2(self):
+        d = self.d * self.powers[self.T[:self.n_taxa, :self.n_taxa]]
+        edges = torch.nonzero(self.solution)
+        # idx_dict = dict(zip([tuple(edge.tolist()) for edge in edges], range(edges.shape[0])))
+        s_mat = self.subtrees_mat[edges[:, 0], edges[:, 1], :].to(torch.int)
+
+        a =s_mat.repeat(s_mat.shape[0],1)
+        b = s_mat.repeat_interleave(s_mat.shape[0], dim=0)
+        res = (1 - (a*b).sum(dim=-1))
+        res[res<0] = 0
+        s = torch.matmul(s_mat[:, :self.n_taxa].flatten().unsqueeze(1), s_mat[:, :self.n_taxa].flatten().unsqueeze(0))
+        dist = (d.repeat(s_mat.shape[0], s_mat.shape[0]) * s).split(self.n_taxa, dim=-1)
+        dist = torch.stack(dist).view(-1, self.n_taxa, self.n_taxa).transpose(1,2)
+
+        return (dist.reshape(-1, self.n_taxa**2).sum(dim=-1) * res).reshape((s_mat.shape[0], s_mat.shape[0]))
+torch.set_printoptions(linewidth=150)
 
 random.seed(0)
 np.random.seed(0)
 
-n = 300
+n = 4
 
 d = np.random.uniform(0,1,(n, n))
 d = np.triu(d) + np.triu(d).T
@@ -197,3 +250,16 @@ print(time.time() - t)
 #         if torch.nonzero(model.subtrees_mat[i, j]).T.squeeze(0).shape[0] > 0:
 #             print(i, j, torch.nonzero(model.subtrees_mat[i, j]).T.squeeze(0))
 
+
+# import torch
+# d = torch.rand((3, 3)).triu()
+# d = d + d.T
+# x = torch.tensor([[1, 1, 0],[0,0,1]])
+# a =x.repeat(2,1)
+# b = x.repeat_interleave(2, dim=0)
+# res = (1 - (a*b).sum(dim=-1))
+# res[res<0] = 0
+# s = torch.matmul(x.flatten().unsqueeze(1), x.flatten().unsqueeze(0))
+#
+# dist = torch.stack((d.repeat(2, 2) * s).split(3, dim=-1)).view(-1, 3, 3).transpose(1,2)
+# f = (dist.reshape(-1, 3*3).sum(dim=-1) * res).reshape((2, 2))
